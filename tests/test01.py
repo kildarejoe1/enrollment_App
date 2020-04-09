636466bc3cde3dcd7d6b7edec0b0c3ba7eb36e57
henry morrin <henry.morrin@iii.com>
2020-04-09 17:19:16 +0100

    added unit tests

--- tests/test01.py
+++ tests/test01.py
@@ -1,3 +1,8 @@
 import unittest
 
 print("This will where I will put unit tests for application")
+
+
+def test_sample_single_word():
+    print("This will where I will put unit tests for application")
+    assert True
