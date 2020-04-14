305bf9530dd45ec90560465abad301845e475bc3
henry morrin <henry.morrin@iii.com>
2020-04-14 11:34:41 +0100

    Build html,css,javascript frontend

--- application/app.py
+++ application/app.py
@@ -1,13 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+#from flask.ext.script import Manager
+#from flask.ext.bootstrap import Bootstrap
 
 
 app=Flask(__name__)
+#manager=Manager(app)
+#bootstrap = Bootstrap(app)
 
-
-@app.route("/")
-def index():
-    return "Hello all"
+@app.route("/login")
+def login():
+    return render_template("login.html",page_title="Login")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(debug=True, port="4000", host="0.0.0.0")
